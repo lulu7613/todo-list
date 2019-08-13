bcedfa965ebb1a35f268861f5da6fbd638afd9c6
蔡昀芷 <yunchihlulu@gmail.com>
2019-08-13 12:00:38 +0800

    finish 頁籤分類

--- js/all.js
+++ js/all.js
@@ -3,11 +3,55 @@ var app = new Vue({
   el: '#app',
   data: {
     newTodo: '',
-
+    todos: [
+      {
+        text: '你好',
+        id: '123',
+        complete: false,
+      }
+    ],
+    visibility: 'all',
   },
   methods: {
-    addTodo: function() {
-
+    addTodo: function () {
+      let value = this.newTodo.trim()
+      let id = Math.floor(Date.now())
+      if (!value) { return }
+      this.todos.push({
+        text: value,
+        id: id,
+        complete: false
+      })
+      this.newTodo = ''
+    },
+    removeTodo: function (index) {
+      this.todos.splice(index, 1)
     }
-  }
+  },
+  computed: {
+    filteredTodos: function () {
+      let newTodos = []
+      let vm = this
+
+      if (this.visibility == 'all') {
+        newTodos = this.todos
+      }
+      else if (this.visibility == 'active') {
+        vm.todos.forEach(function (item) {
+          if (!item.complete) {
+            newTodos.push(item)
+          }
+        });
+      }
+      else if (this.visibility == 'completed') {
+        vm.todos.forEach(function (item) {
+          if (item.complete) {
+            newTodos.push(item)
+          }
+        });
+      }
+      
+      return newTodos
+    }
+  },
 })
